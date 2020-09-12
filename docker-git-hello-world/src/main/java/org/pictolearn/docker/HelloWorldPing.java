7bb368baf3bb8ed2e6cc3053c065ce78c6f911e5
arvindk999 <arvindk999@gmail.com>
2020-09-12 11:07:34 +0530

    Changed Name and sleep time

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -3,8 +3,8 @@ package org.pictolearn.docker;
 public class HelloWorldPing {
 	public static void main(String args[]) throws Exception {
 		for (int i = 0; i < 100; i++) {
-			System.out.println("Hello World Ping " + i);
-			Thread.sleep(1000);
+			System.out.println("Hello Kushi Ping " + i);
+			Thread.sleep(1500);
 		}
 	}
 }
